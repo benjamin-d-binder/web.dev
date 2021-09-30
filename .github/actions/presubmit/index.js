210941c3ff775480242a339f9aa8d6772a07a7fd
Michael Solati <mkslt04@gmail.com>
2021-09-30 12:38:20 -0700

    fix(deploy): use warning instead of notice

--- .github/actions/presubmit/index.js
+++ .github/actions/presubmit/index.js
@@ -36,10 +36,10 @@ async function run() {
       });
       core.info(`Succeeded to remove label: ${LABEL}`);
     } catch (e) {
-      core.notice(`Failed to remove label: ${LABEL}`);
+      core.warning(`Failed to remove label: ${LABEL}`);
     }
   } else {
-    core.notice(`Pull request does not have label: ${LABEL}`);
+    core.warning(`Pull request does not have label: ${LABEL}`);
   }
 
   core.setOutput('isPresubmit', isPresubmit);
